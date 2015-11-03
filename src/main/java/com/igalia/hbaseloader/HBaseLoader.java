a13e7a6177b7e7c9b8a33d73ff311b0ed74cb88d
Diego Pino Garcia <dpino@igalia.com>
2015-11-03 10:17:50 +0100

    Remove tabs and trailing whitespaces

--- src/main/java/com/igalia/hbaseloader/HBaseLoader.java
+++ src/main/java/com/igalia/hbaseloader/HBaseLoader.java
@@ -11,66 +11,66 @@ import com.igalia.hbaseloader.utils.HBaseHelper;
 
 /**
  * Loads files into a table:
- * 	* Tablename: 'files'
- * 	* ColumnFamiy: 'content:'
- * 
+ *     * Tablename: 'files'
+ *     * ColumnFamiy: 'content:'
+ *
  * Use:
- * 	HBaseLoader <tableName> <dir>
- * 
+ *     HBaseLoader <tableName> <dir>
+ *
  * @params tableName Name of the table where to import files
  * @params dir Directory containing files to import
- * 
+ *
  * Diego Pino García <dpino@igalia.com>
  *
  */
 public class HBaseLoader {
 
-	private static final HBaseHelper hbase =  HBaseHelper.create();
-	
-    public static void main(String[] args) {    	
-    	if (args.length < 1 || args.length > 2) {
-    		error("HBaseLoader <tablename> <dir>");
-    		return;
-    	}
-		if (hbase == null) {
-			error("Couldn't establish connection to HBase");
-		}
+    private static final HBaseHelper hbase =  HBaseHelper.create();
 
-		String fileName = args[1];
-		File file = new File(fileName);
-		if (file == null) {
-			error(String.format("Directory %s doesn't exist", fileName));
-		}
-		if (!file.isDirectory()) {
-			error(String.format("File %s is not a directory", fileName));
-		}
-		
-		HTable table;
-		try {
-			String tableName = args[0];
-			table = hbase.getOrCreateTable(tableName, "content");
-			for (File each: file.listFiles()) {
-				if (each.isFile()) {
-					insertInto(table, each);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 2) {
+            error("HBaseLoader <tablename> <dir>");
+            return;
+        }
+        if (hbase == null) {
+            error("Couldn't establish connection to HBase");
+        }
+
+        String fileName = args[1];
+        File file = new File(fileName);
+        if (file == null) {
+            error(String.format("Directory %s doesn't exist", fileName));
+        }
+        if (!file.isDirectory()) {
+            error(String.format("File %s is not a directory", fileName));
+        }
+
+        HTable table;
+        try {
+            String tableName = args[0];
+            table = hbase.getOrCreateTable(tableName, "content");
+            for (File each: file.listFiles()) {
+                if (each.isFile()) {
+                    insertInto(table, each);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-	private static void insertInto(HTable table, File file) {
-		try {
-			byte[] content = Bytes.toBytes(FileUtils.readFileToString(file, "UTF-8"));
-			hbase.insert(table, file.getName(), "content", "", content);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static void insertInto(HTable table, File file) {
+        try {
+            byte[] content = Bytes.toBytes(FileUtils.readFileToString(file, "UTF-8"));
+            hbase.insert(table, file.getName(), "content", "", content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private static void error(String str) {
-		System.out.println("Error: " + str);
-	}
-    
+    private static void error(String str) {
+        System.out.println("Error: " + str);
+    }
+
 }
--- src/main/java/com/igalia/hbaseloader/utils/HBaseHelper.java
+++ src/main/java/com/igalia/hbaseloader/utils/HBaseHelper.java
@@ -17,103 +17,103 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
 /**
- * 
+ *
  * @author Diego Pino García <dpino@igalia.com>
- * 
+ *
  */
 public class HBaseHelper {
 
-	private static final Configuration conf = HBaseConfiguration.create();
-	private static final HTablePool tablePool = new HTablePool(conf, Integer.MAX_VALUE);
+    private static final Configuration conf = HBaseConfiguration.create();
+    private static final HTablePool tablePool = new HTablePool(conf, Integer.MAX_VALUE);
 
-	public static HBaseHelper create() {
-		HBaseHelper result = new HBaseHelper();
-		try {
-			result.hbase = new HBaseAdmin(conf);
-			return result;
-		} catch (MasterNotRunningException e) {
-			e.printStackTrace();
-		} catch (ZooKeeperConnectionException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public static HBaseHelper create() {
+        HBaseHelper result = new HBaseHelper();
+        try {
+            result.hbase = new HBaseAdmin(conf);
+            return result;
+        } catch (MasterNotRunningException e) {
+            e.printStackTrace();
+        } catch (ZooKeeperConnectionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	private HBaseAdmin hbase;
+    private HBaseAdmin hbase;
 
-	static {
-		conf.set("hbase.master", "localhost:60000");
-	}
+    static {
+        conf.set("hbase.master", "localhost:60000");
+    }
 
-	private HBaseHelper() {
+    private HBaseHelper() {
 
-	}	
+    }
 
-	public HTable createTable(String tableName, String... descriptors)
-			throws IOException {
-		if (tableExists(tableName)) {
-			dropTable(tableName);
-		}
-		return doCreateTable(tableName, descriptors);
-	}
+    public HTable createTable(String tableName, String... descriptors)
+            throws IOException {
+        if (tableExists(tableName)) {
+            dropTable(tableName);
+        }
+        return doCreateTable(tableName, descriptors);
+    }
 
-	private HTable doCreateTable(String tableName, String... descriptors)
-			throws IOException {
-		HTableDescriptor descriptor = new HTableDescriptor(tableName);
-		for (String each : descriptors) {
-			HColumnDescriptor cd = new HColumnDescriptor(each.getBytes());
-			descriptor.addFamily(cd);
-		}
-		hbase.createTable(descriptor);
-		debug(String.format("Database %s created", tableName));
-		return new HTable(tableName);
-	}
-	
+    private HTable doCreateTable(String tableName, String... descriptors)
+            throws IOException {
+        HTableDescriptor descriptor = new HTableDescriptor(tableName);
+        for (String each : descriptors) {
+            HColumnDescriptor cd = new HColumnDescriptor(each.getBytes());
+            descriptor.addFamily(cd);
+        }
+        hbase.createTable(descriptor);
+        debug(String.format("Database %s created", tableName));
+        return new HTable(tableName);
+    }
+
     private static void debug(Object obj) {
         System.out.println(String.format("### DEBUG: %s", obj.toString()));
-    }    
+    }
 
-	public void dropTable(String tableName) throws IOException {
-		hbase.disableTable(tableName);
-		hbase.deleteTable(tableName);
-	}
+    public void dropTable(String tableName) throws IOException {
+        hbase.disableTable(tableName);
+        hbase.deleteTable(tableName);
+    }
 
-	public HTable getOrCreateTable(String tableName, String... descriptors)
-			throws IOException {
-		if (!tableExists(tableName)) {
-			doCreateTable(tableName, descriptors);
-		}
-		return getTable(tableName);
-	}
-	
-	public HTable getTable(String tableName) {
-		return (HTable) tablePool.getTable(tableName);
-	}
+    public HTable getOrCreateTable(String tableName, String... descriptors)
+            throws IOException {
+        if (!tableExists(tableName)) {
+            doCreateTable(tableName, descriptors);
+        }
+        return getTable(tableName);
+    }
 
-	public void insert(HTable table, String rowKey, List<String> values)
-			throws IOException {
-		if (values.size() == 3) {
-			Put put = new Put(Bytes.toBytes(rowKey));
-			put.add(Bytes.toBytes(values.get(0)), Bytes.toBytes(values.get(1)),
-					Bytes.toBytes(values.get(2)));
-			table.put(put);
-		}
-	}
+    public HTable getTable(String tableName) {
+        return (HTable) tablePool.getTable(tableName);
+    }
 
-	public void insert(HTable table, String rowKey, String prefix, String qualifier, byte[] value)
-			throws IOException {
-		Put put = new Put(Bytes.toBytes(rowKey));
-		put.add(Bytes.toBytes(prefix), Bytes.toBytes(qualifier), value);
-		table.put(put);
-	}
-	
-	public boolean tableExists(String tableName) {
-		try {
-			return hbase.tableExists(tableName);
-		} catch (IOException e) {			
-			e.printStackTrace();
-		}
-		return false;
-	}
+    public void insert(HTable table, String rowKey, List<String> values)
+            throws IOException {
+        if (values.size() == 3) {
+            Put put = new Put(Bytes.toBytes(rowKey));
+            put.add(Bytes.toBytes(values.get(0)), Bytes.toBytes(values.get(1)),
+                    Bytes.toBytes(values.get(2)));
+            table.put(put);
+        }
+    }
+
+    public void insert(HTable table, String rowKey, String prefix, String qualifier, byte[] value)
+            throws IOException {
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.add(Bytes.toBytes(prefix), Bytes.toBytes(qualifier), value);
+        table.put(put);
+    }
+
+    public boolean tableExists(String tableName) {
+        try {
+            return hbase.tableExists(tableName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
